183de5c0c1344143e055afb5b3b46799d0ecc45c
Allen Kim <allen.kim@rci.rogers.ca>
2018-05-31 14:11:16 -0400

    updated

--- src/hello-custom-element/hello-custom-element.js
+++ src/hello-custom-element/hello-custom-element.js
@@ -12,24 +12,30 @@ const template = require('./hello-custom-element.html');
 
 // private variables
 function __addEventListener() {
-  this.querySelector('.fire-event').addEventListener('click', e => {
-    this.dispatchEvent(new CustomEvent('my-event', {
-      bubbles: true, detail: new Date()
-    }));
-  });
+  this.querySelector('.fire-event').
+    addEventListener('click', e => {
+      this.dispatchEvent(new CustomEvent('my-event', {
+        bubbles: true, detail: new Date()
+      }));
+    });
+  this.querySelector('.run-ng-func').
+    addEventListener('click', this.runNgFunc.bind(this));
 }
 
 class HelloCustomElement extends HTMLElement {
   connectedCallback() {
     this.options = util.attrs2Options(this.attributes);
+    console.log('this.options', this.options);
     this.innerHTML = Mustache.to_html(template, this.options);
-    this.setAttribute('custom-element', '');
     __addEventListener.bind(this)();
   }
 
-  updateNotice(notice) {
-    this.querySelector('.notice-msg').innerHTML = notice;
+  updateMessage(message) {
+    this.querySelector('.message').innerHTML = message;
   }
 
+  runNgFunc(e) {
+    this.options.ngFunc();
+  }
 }
 customElements.define('hello-custom-element', HelloCustomElement);
