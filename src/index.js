8043bd478de5d030ae5d124739fb6906c21be568
Allen Kim <allen.kim@rci.rogers.ca>
2018-06-01 10:58:52 -0400

    updated with CustomEvent Polyfill

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 // import './custom-element-polyfill.js';
+// new CustomEvent not working on IE11.
+(function () {
+
+  if ( typeof window.CustomEvent === "function" ) return false;
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
+
 import 'document-register-element';
 import './hello-custom-element/hello-custom-element.js';
