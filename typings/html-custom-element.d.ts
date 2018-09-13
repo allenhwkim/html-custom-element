ed6239e4c941c089f7d135c1a0dabc28454c3a4f
Allen Kim <allen.kim@rci.rogers.ca>
2018-09-13 13:36:12 -0400

    updated typings

--- typings/html-custom-element.d.ts
+++ typings/html-custom-element.d.ts
@@ -2,5 +2,5 @@ import 'document-register-element';
 export declare class HTMLCustomElement extends HTMLElement {
     static define(tagName: any, klass: any): void;
     disconnectedCallback(): void;
-    render(): any;
+    render(): Promise<any>;
 }
